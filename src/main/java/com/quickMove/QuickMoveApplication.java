606f13de8bc3a140823a07677c12533c8b9c4abe
I528622 <bikramkumar080@gmail.com>
2024-11-06 15:56:50 +0530

    spring

---
+++ src/main/java/com/quickMove/QuickMoveApplication.java
@@ -0,0 +1,13 @@
+package com.quickMove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickMoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickMove/QuickMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickMove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickMoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
